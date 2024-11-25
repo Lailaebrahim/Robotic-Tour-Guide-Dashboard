c88db0d420651456d78cf106074afea1cc42703e
Lailaebrahim <ebrahimlaila815@gmail.com>
2024-11-25 19:11:03 +0200

    MongoDB Connection Client

---
+++ backend/utils/dbClient.js
@@ -0,0 +1,46 @@
+/**
+ * DBClient class to handle MongoDB connection.
+ */
+import mongoose from "mongoose";
+
+
+class DBClient {
+    /**
+     * constructor for the DBClient class.
+     * sets the URI for the MongoDB connection and calls the connect method
+     * to establish a connection.
+     */
+    constructor() {
+        this.uri = process.env.DB_URI;
+        this.connection = null;
+        this.connect();
+    }
+
+    /**
+     * connects to the MongoDB database using mongoose.
+     * logs a success message if connected, otherwise logs an error.
+     * @async
+     * @returns {Promise<void>}
+     */
+    async connect() {
+        try {
+            this.connection = await mongoose.connect(this.uri, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            });
+            console.log('MongoDB connected successfully');
+        } catch (error) {
+            console.error(`MongoDB connection error: ${error}`);
+        }
+    }
+
+    /**
+     * checks if the MongoDB connection is alive.
+     * @returns {boolean} true if the connection is alive, otherwise false.
+     */
+    isAlive() {
+        return mongoose.connection.readyState === 1;
+    }
+}
+
+export default DBClient;
