b6aa367230c1a8d997fe65c4f3161671a1542805
Lailaebrahim <ebrahimlaila815@gmail.com>
2024-11-25 16:48:40 +0200

    Backend Server Entry Point

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+/**
+ * @fileoverview Entry point for the Robotic Tour Guide Dashboard backend server.
+ * Sets up the Express server, middleware, and routes.
+ *
+ * @requires express
+ * @requires dotenv
+ * @requires jsend
+ * @requires cookie-parser
+ */
+import express from "express";
+import dotenv from "dotenv";
+import jsend from "jsend";
+import cookieParser from "cookie-parser";
+
+// express application instance
+const app = express();
+
+// add env variables to process.env
+dotenv.config();
+
+// middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(jsend.middleware);
+
+// route handler for undefined routes
+app.use("*", (_, res) => {
+  res.status(404).jsend.fail({ message: "Url Not Found" });
+});
+
+// starts the server and listens on the specified port
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
