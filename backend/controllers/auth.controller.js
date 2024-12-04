915294395b5df804eb4dfa273c8c88fb169b30fc
Lailaebrahim <ebrahimlaila815@gmail.com>
2024-12-04 18:44:24 +0200

    add controller for checking auth route & request content validation

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import asyncHandler from "express-async-handler";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { validationResult } from "express-validator";
 import User from "../models/user.model.js";
 import AppError from "../utils/error.js";
 import path from "path";
@@ -20,15 +21,20 @@ import {
 import { userRoles, rolePermissions } from "../utils/userRolesPermissions.js";
 
 export const signUpUser = asyncHandler(async (req, res, next) => {
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    const errorMessages = result.array().map(error => error.msg).join("\n");
+    return next(new AppError(400, errorMessages));
+  }
   const { firstName, lastName, email, username, password, role } = req.body;
 
   const existingUser = await User.findOne({ $or: [{ email }, { username }] });
   if (existingUser) {
-    const error = new AppError(400, "User already exists !");
+    const error = new AppError(400, "User exists, ");
     if (existingUser.email == email) {
-      error.details = { email: "Email already registered !" };
+      error.message += "Email already registered !";
     } else {
-      error.details = { username: "Username already taken !" };
+      error.message += "Username taken !";
     }
     return next(error);
   }
@@ -55,12 +61,10 @@ export const signUpUser = asyncHandler(async (req, res, next) => {
 
   await sendVerificationEmail(user.email, user.verificationToken);
 
-  res.status(201).jsend.success({
+  res.status(201).send({
     message:
       "User created successfully, please check your email for verification",
-    email: user.email,
-    username: user.username,
-    status: user.status,
+    user,
     verificationToken: user.verificationToken,
     verificationTokenExpAt: user.verificationTokenExpAt,
   });
@@ -92,12 +96,21 @@ export const verifyEmail = asyncHandler(async (req, res, next) => {
   res.jsend.success({
     message: "Email verified successfully",
     activationToken: activationToken,
+    user,
   });
 });
 
 export const accountCompletion = asyncHandler(async (req, res, next) => {
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    const errorMessages = result.array().map(error => error.msg).join("\n");
+    return next(new AppError(400, errorMessages));
+  }
   const { firstName, lastName, password } = req.body;
   const activationToken = req.params.token;
+  if (!activationToken) {
+    return next(new AppError(400, "Activation token is required"));
+  }
   try {
     const decodedJWT = jwt.verify(
       activationToken,
@@ -105,6 +118,9 @@ export const accountCompletion = asyncHandler(async (req, res, next) => {
     );
     const user = await User.findById(decodedJWT._id);
     if (!user) return next(new AppError(403, "Forbidden No user found"));
+    if (user.status != "pending") return next(
+      new AppError(403, "Invalid activation token")
+    );
 
     user.firstName = firstName;
     user.lastName = lastName;
@@ -116,13 +132,15 @@ export const accountCompletion = asyncHandler(async (req, res, next) => {
     user.status = "active";
     await user.save();
 
+    await sendWelcomeEmail(user.email, user.username);
+
     res.jsend.success({
       message:
         "Your Account Creation is Completed Successfully! You can login to your account now.",
     });
   } catch (err) {
     if (err.name === "TokenExpiredError") {
-      return next(new AppError(401, "Access token expired please login again"));
+      return next(new AppError(401, "Activation token expired request another one"));
     } else {
       return next(
         new AppError(403, "Forbidden Invalid activation token: " + err.message)
@@ -132,6 +150,11 @@ export const accountCompletion = asyncHandler(async (req, res, next) => {
 });
 
 export const loginUser = asyncHandler(async (req, res, next) => {
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    const errorMessages = result.array().map(error => error.msg).join("\n");
+    return next(new AppError(400, errorMessages));
+  }
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (!user) {
@@ -149,15 +172,14 @@ export const loginUser = asyncHandler(async (req, res, next) => {
   user.refreshToken = refreshToken;
   user.lastLogin = Date.now();
   await user.save();
+  const userWithoutSensitiveInfo = user.toObject();
+  delete userWithoutSensitiveInfo.password;
+  delete userWithoutSensitiveInfo.activationToken;
+
   res.jsend.success({
     message: "Login successful",
     accessToken,
-    user: {
-      email: user.email,
-      username: user.username,
-      role: user.role,
-      lastLogin: user.lastLogin,
-    },
+    user: userWithoutSensitiveInfo
   });
 });
 
@@ -173,7 +195,7 @@ export const refreshAccessToken = asyncHandler(async (req, res, next) => {
     if (user.status !== "active")
       return next(new AppError(403, "Forbidden User not active"));
     const { accessToken, _ } = generateAccessRefreshToken(user);
-    res.jsend.success({ email: user.email, accessToken: accessToken });
+    res.jsend.success({ user, accessToken: accessToken });
   } catch (err) {
     if (err.name === "TokenExpiredError") {
       return next(
@@ -205,6 +227,7 @@ export const logOutUser = asyncHandler(async (req, res, next) => {
     }
   }
   res.clearCookie("JWT");
+  req.user = null;
   res.jsend.success({ message: "Logout successful" });
 });
 
@@ -252,4 +275,15 @@ export const resetPassword = asyncHandler(async (req, res, next) => {
   await sendPasswordResetSuccessEmail(user.email);
 
   res.jsend.success({ message: "Password reset successfully" });
+});
+
+export const checkAuth = asyncHandler(async (req, res, next) => {
+  if (!req.user) {
+    return next(new AppError(401, "Unauthorized no req.user"));
+  }
+  const user = await User.findById(req.user.id).select("-password").select("-refreshToken");
+  if (!user) {
+    return next(new AppError(401, "Unauthorized no user"));
+  }
+  res.jsend.success({ message: "Authorized", user });
 });
