d3ac683b09fbd2db74f2a3ffa4e1768d469a5d46
Lailaebrahim <ebrahimlaila815@gmail.com>
2025-01-04 23:32:43 +0200

    add cors and morgan middleware and tours route

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,16 @@ import express from "express";
 import dotenv from "dotenv";
 import jsend from "jsend";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
+import WebSocketServer from "ws";
 import errorHandler from "./middlewares/error/errorHandler.js";
 import DBClient from "./utils/dbClient.js";
 import rosController from "./utils/rosClient.js";
 import authRouter from "./routes/auth.routes.js";
 import adminRouter from "./routes/admin.routes.js";
 import robotRouter from "./routes/robot.routes.js";
+import tourRoutes from "./routes/tour.routes.js";
 
 // express application instance
 const app = express();
@@ -25,6 +29,8 @@ const app = express();
 dotenv.config();
 
 // middlewares
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 app.use(jsend.middleware);
@@ -33,6 +39,7 @@ app.use(jsend.middleware);
 app.use("/api/auth", authRouter);
 app.use("/api/admin", adminRouter);
 app.use("/api/robot", robotRouter);
+app.use("/api/tours", tourRoutes);
 
 // route handler for undefined routes
 app.use("*", (_, res) => {
@@ -43,7 +50,7 @@ app.use("*", (_, res) => {
 app.use(errorHandler);
 
 // starts the server and listens on the specified port
-app.listen(process.env.PORT || 5000, () => {
+const server = app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
  new DBClient();
   // initialize connection
@@ -54,4 +61,9 @@ app.listen(process.env.PORT || 5000, () => {
       console.error("Initial connection failed:", JSON.stringify(error.message));
     }
   })();
+});
+
+export const wss = new WebSocketServer.Server({ server });
+wss.on('connection', (ws) => {
+  console.log('New frontend client connected');
 });
